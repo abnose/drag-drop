5ce8e02b893567aeedd97fd09ea52948d6c92801
e.karimi <abnoseek@gmail.com>
2024-08-27 15:11:08 +0330

    feat: adding date picker and primevue packages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import "primeicons/primeicons.css";
+import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+
+const app = createApp(App);
+app
+  .use(pinia)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+  })
+  .use(Vue3PersianDatetimePicker, {
+    name: "CustomDatePicker",
+    props: {
+      format: "YYYY-MM-DD HH:mm",
+      displayFormat: "jYYYY-jMM-jDD",
+      editable: false,
+      inputClass: "form-control",
+      placeholder: "لطفا تاریخ را وارد کنید",
+      altFormat: "YYYY-MM-DD HH:mm",
+      color: "#00acc1",
+      autoSubmit: false,
+    },
+  })
+  .mount("#app");
