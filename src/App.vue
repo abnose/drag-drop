d1edec58b8006da83757d33d09a20515212f1c74
e.karimi <abnoseek@gmail.com>
2024-08-27 15:10:43 +0330

    update: adding variables and styles

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,61 @@
 <script setup lang="ts">
+import Button from 'primevue/button';
 import { ref , watch } from "vue";
 import { VueDraggableNext } from "vue-draggable-next";
+import dataStore from './store/crud'
+import { getRandomInt } from "./utils/util";
+const store = dataStore()
 
 // Define the meals and yuckyMeals arrays
-const data = ref(
-  [
-    { header: 'header 1',
-      id: 1, 
-    body :[
-      {comment: 'comment 1 1', createDate: '1 1' , deadline: '1 1'},
-      {comment: 'comment 1 2', createDate: '1 2' , deadline: '1 2'},
-      {comment: 'comment 1 3', createDate: '1 3' , deadline: '1 3'},
-      {comment: 'comment 1 4', createDate: '1 4' , deadline: '1 4'},
-    ]},
-    { header: 'header 2',
-      id: 2, 
-    body :[
-      {comment: 'comment 2 1', createDate: '2 1' , deadline: '2 1'},
-      {comment: 'comment 2 2', createDate: '2 2' , deadline: '2 2'},
-      {comment: 'comment 2 3', createDate: '2 3' , deadline: '2 3'},
-      {comment: 'comment 2 4', createDate: '2 4' , deadline: '2 4'},
-    ]}
-])
+// const data = ref(
+//   [
+//     { header: 'header 1',
+//       id: 1, 
+//     body :[
+//       {comment: 'comment 1 1', createDate: '1 1' , deadline: '1 1'},
+//       {comment: 'comment 1 2', createDate: '1 2' , deadline: '1 2'},
+//       {comment: 'comment 1 3', createDate: '1 3' , deadline: '1 3'},
+//       {comment: 'comment 1 4', createDate: '1 4' , deadline: '1 4'},
+//     ]},
+//     { header: 'header 2',
+//       id: 2, 
+//     body :[
+//       {comment: 'comment 2 1', createDate: '2 1' , deadline: '2 1'},
+//       {comment: 'comment 2 2', createDate: '2 2' , deadline: '2 2'},
+//       {comment: 'comment 2 3', createDate: '2 3' , deadline: '2 3'},
+//       {comment: 'comment 2 4', createDate: '2 4' , deadline: '2 4'},
+//     ]}
+// ])
+
+const data = ref(store.storedData)
 
 watch(data.value, async () => {
-  console.log(data.value)
+  store.saveData(data.value)
 })
 
+const addTaskGroup = () => {
+  data.value.push(
+    { 
+      header: getRandomInt(5000),
+      id: getRandomInt(5000), 
+      body :[]
+    }
+  )
+}
 
+const test = ref()
 
 </script>
 
 <template>
+  <!-- <Button class="customBtn">+</Button> -->
+   <div class="helperBtnGroup">
+     <Button  @click="addTaskGroup" icon="pi pi-plus" aria-label="Save" />
+  </div>
+  <custom-date-picker
+   v-model="test"
+
+ />
   <div v-for="item in data" class="">
 
     <h1>{{ item.header }}</h1>
@@ -44,7 +68,18 @@ watch(data.value, async () => {
 
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.helperBtnGroup {
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transform: translate(15% ,15%);
+}
 li {
   list-style: none;
   padding: 8px;
