11f0266f95cc07925ac3430e48cd37d20c12d842
Erfan nejat <abnoseek@gmail.com>
2024-08-29 11:40:48 +0330

    bug: fix date showing persian and remove comments

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,15 +1,8 @@
-import moment from "moment-jalaali";
 export function getRandomInt(max: number): number {
   return Math.floor(Math.random() * max);
 }
 
 export function handleDate(dateString: string): string {
-  // Parse the input date string into a moment object
-  const date = moment(dateString);
-
-  // Convert the moment object to Jalali date format
-  const persianDate = date.format("dddd D MMMM");
-
-  // Return the Persian date
-  return persianDate;
+  let createDate = new Date(dateString).toLocaleDateString('fa-IR', { year: 'numeric', month: 'long', day: 'numeric' });
+  return createDate
 }
